8b85924e994a6b4f7e51181b5651ad1f9d68bcfe
fer g <arielgeronimo517@gmail.com>
2020-07-23 19:54:12 -0600

    Creando el state para las citas en el componente Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,21 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useState } from "react"
 
 const Formulario = () => {
+
+//Creando state de citas
+  const [cita, actualizarCita] = useState({
+      mascota:'',
+      propietario:'',
+      fecha:'',
+      hora:'',
+      sintomas:''
+  })
+
+  //Funcion que se ejecuta cada vez que el usuario escribe en un input
+  const actualizarState = () => {
+    console.log("escribiendo...")
+  }
+
   return(
     <Fragment>
       <h1>Nueva Cita</h1>
@@ -11,6 +26,7 @@ const Formulario = () => {
             name="mascota"
             className="u-full-width"
             placeholder="Nombre Mascota"
+            onChange={actualizarState}
           />
           <label>Propietario</label>
           <input
@@ -18,23 +34,27 @@ const Formulario = () => {
               name="propietario"
               className="u-full-width"
               placeholder="Nombre Propietario"
+              onChange={actualizarState}
             />
             <label>Fecha</label>
             <input
                 type="date"
                 name="fecha"
                 className="u-full-width"
+                onChange={actualizarState}
               />
               <label>Hora</label>
               <input
                   type="time"
                   name="hora"
                   className="u-full-width"
+                  onChange={actualizarState}
                 />
                 <label>Sintomas</label>
                 <textarea
                     name="sintomas"
                     className="u-full-width"
+                    onChange={actualizarState}
                   ></textarea>
                   <button
                     type="submit"
